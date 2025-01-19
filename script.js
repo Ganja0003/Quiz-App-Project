7a27308af00c5626457b78a7db92ce5d6d53c7a8
yuusuf <yuusuf98@outlook.com>
2025-01-19 10:33:44 +0100

    I have removed the array of objects of questions and instead fethed the data from my newly made api.

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
-const quizQuestions = [
-  {
-    id: 1,
-    question: "What is the book sent to the Muslims called?",
-    options: [
-      { text: "Sunnah", isCorrect: false },
-      { text: "Quran", isCorrect: true },
-      { text: "Hadith", isCorrect: false },
-      { text: "Wudhu", isCorrect: false },
-    ],
-    explanation: "The book that is sent to the Muslims is called Quran.",
-  },
-  {
-    id: 2,
-    question: "What is the name of the last prophet in Islam?",
-    options: [
-      { text: "Musa", isCorrect: false },
-      { text: "Issa", isCorrect: false },
-      { text: "Muhammad", isCorrect: true },
-      { text: "Ibrahim", isCorrect: false },
-    ],
-    explanation: "The last prophet in Islam is Prophet Muhammad (peace be upon him).",
-  },
-  {
-    id: 3,
-    question: "What is the primary place of worship for Muslims?",
-    options: [
-      { text: "Church", isCorrect: false },
-      { text: "Synagog", isCorrect: false },
-      { text: "Mosque", isCorrect: true },
-      { text: "Temple", isCorrect: false },
-    ],
-    explanation: "The primary place of worship for Muslims is a mosque.",
-  },
-];
-
+let quizQuestions = [];
 let player1Score = 0;
 let player2Score = 0;
 let currentPlayer = 1;
@@ -50,7 +15,6 @@ const playerForm = document.getElementById('playerForm');
 const player1UI = document.getElementById('player1UI');
 const player2UI = document.getElementById('player2UI');
 
-
 playerForm.addEventListener("submit", function(event){
   event.preventDefault();
   const player1ValueName = document.getElementById('player1ValueName').value;
@@ -62,6 +26,19 @@ playerForm.addEventListener("submit", function(event){
   playerForm.classList.add('hidden');
 });
 
+async function fetchQuizData() {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/Ganja0003/Ganja0003.github.io/refs/heads/main/apis/data.json');
+    if (!response.ok) {
+      return(`There is an error fetching the quiz data: ${response.statusText}`);
+    }
+    quizQuestions = await response.json();
+    displayQuestion();
+  } catch (err) {
+    console.error(err.message);
+    alert('Failed, cant load quiz data');
+  }
+}
 
 function displayQuestion() {
   const currentQuestion = quizQuestions[currentQuestionIndex];
@@ -146,11 +123,9 @@ function showQuestions() {
   
 }
 
-
 function revealAnswer(index) {
   document.getElementById(`answer-${index}`).style.display = 'block';
 }
-
 
 function stateWinner() {
   const winningMessage = document.createElement('p');
@@ -179,5 +154,5 @@ function filterQuestions(searchTerm) {
   });
 }
 
-displayQuestion();
+fetchQuizData();
 form.addEventListener('submit', submitForm);
